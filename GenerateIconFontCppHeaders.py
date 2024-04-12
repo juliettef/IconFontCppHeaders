5f08630efa3d6729a1737990bf944b3a79da1569
juliettef <juliette@enkisoftware.com>
2024-04-12 15:10:00 +0100

    Lucide alt jsDelivr link - PR #41

--- GenerateIconFontCppHeaders.py
+++ GenerateIconFontCppHeaders.py
@@ -456,8 +456,8 @@ class FontLC( FontKI ):               # Lucide
     font_name = 'Lucide'
     font_abbr = 'LC'
     font_data_prefix = '.icon-'
-    font_data = 'https://unpkg.com/lucide-static@latest/font/lucide.css'
-    ttfs = [[ font_abbr, 'lucide.ttf', 'https://unpkg.com/lucide-static@latest/font/lucide.ttf' ]]
+    font_data = 'https://unpkg.com/lucide-static@latest/font/lucide.css'    # alt 'https://cdn.jsdelivr.net/npm/lucide-static@latest/font/lucide.css'
+    ttfs = [[ font_abbr, 'lucide.ttf', 'https://unpkg.com/lucide-static@latest/font/lucide.ttf' ]]  # alt 'https://cdn.jsdelivr.net/npm/lucide-static@latest/font/lucide.ttf'
 
 
 # Languages
